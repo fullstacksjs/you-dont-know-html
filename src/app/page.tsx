f5369aa375a8f68d254f2593c3ebd2ff7433668d
Alireza Safaierad <frontendmonster@gmail.com>
2025-02-28 17:44:22 +0100

    refactor: improve configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { CleanStorage } from "@/components/clear-storage";
 import { Welcome } from "@/components/welcome";
 
 export default function HomePage() {
-  return (
-    <>
-      <Welcome />
-      <CleanStorage />
-    </>
-  );
+  return <Welcome />;
 }
