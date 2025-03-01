80f7a12dd1a9c2b4d76bec14d8b35878fa208434
Alireza Safaierad <frontendmonster@gmail.com>
2025-03-01 19:17:41 +0100

    chore: remove welcome component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import { Welcome } from "@/components/welcome";
+import { RestartButton } from "@/components/restart-button";
 
 export default function HomePage() {
-  return <Welcome />;
+  return (
+    <main className="flex h-[100dvh] flex-col justify-center p-4">
+      <div className="flex h-full flex-col gap-10 justify-center">
+        <h2 className="text-center text-4xl/normal font-bold">
+          {"YOU DON’T KNOW "}
+          <span className="text-accent block">{"HTML"}</span>
+        </h2>
+      </div>
+      <div className="flex flex-col justify-end">
+        <RestartButton />
+      </div>
+    </main>
+  );
 }
