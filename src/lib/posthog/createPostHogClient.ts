c07a06131bdb25d16a80e49e9eecdb50760ca0fc
Alireza Safaierad <frontendmonster@gmail.com>
2025-04-11 22:12:50 +0200

    fix: fix build error

--- src/lib/posthog/createPostHogClient.ts
+++ src/lib/posthog/createPostHogClient.ts
@@ -4,7 +4,10 @@ import { PostHog } from "posthog-node";
 import { configSchema } from "./config";
 
 export default function createPostHogClient() {
-  return new PostHog(configSchema.get("key"), {
+  const key = configSchema.get("key");
+  if (!key) return undefined;
+
+  return new PostHog(key, {
     host: configSchema.get("host"),
     flushAt: 1,
     flushInterval: 0,
