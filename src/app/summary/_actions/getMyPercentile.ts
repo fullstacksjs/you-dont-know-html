994bd7206aa1b5bac76fe2613d6fccb76dcea8f8
Alireza Safaierad <frontendmonster@gmail.com>
2025-04-12 01:38:02 +0200

    fix: fix percentile

--- src/app/summary/_actions/getMyPercentile.ts
+++ src/app/summary/_actions/getMyPercentile.ts
@@ -1,8 +1,9 @@
 "use server";
+import { questions } from "@app/questions/questions";
 import { getGamesCountWithCorrectAnswers } from "@app/supabase/supabase";
 import { percent } from "@fullstacksjs/toolbox";
 
 export async function getMyPercentile(correctAnswers: number) {
   const gamesCount = await getGamesCountWithCorrectAnswers(correctAnswers);
-  return percent(gamesCount, correctAnswers);
+  return percent(gamesCount, questions.length);
 }
